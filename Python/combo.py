04a590f212de1aabffa6150676f116cf2a569eb3
Nick Pourazima <npourazima@gmail.com>
2018-06-19 13:11:43 -0400

    +finalDataCombine

--- Python/combo.py
+++ Python/combo.py
@@ -2,10 +2,10 @@ import os
 import glob
 import pandas as pd
 
-currentPath = '/Users/nickpourazima/GitHub/he-sm/TestOutput/Temp'
+currentPath = '/Users/nickpourazima/GitHub/he-sm/TestOutput/Temp/Temp2'
 all_files = glob.iglob(os.path.join(currentPath, "*.csv"))
 summary = pd.concat((pd.read_csv(f, skipinitialspace=True) for f in all_files), ignore_index=True)
-summaryName = ('CombinedToday')
+summaryName = ('Combo_Final')
 summaryPath = os.path.join(currentPath, summaryName)
 summary.to_csv(summaryPath+'.csv')
 # summary.to_excel(summaryPath+'.xlsx')
